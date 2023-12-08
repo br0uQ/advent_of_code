48549592f026a23bf9b2d778e49628a38861c899
Joshua Schmucker <joshua.schmucker@gmail.de>
2023-12-08 10:35:12 +0100

    Rework of day02 part 1: for easier reuse for part 2

--- src/day_02.rs
+++ src/day_02.rs
@@ -14,6 +14,46 @@ pub fn run_part(input: String, part: i8) {
     }
 }
 
+fn get_number_and_rounds(line: &str) -> (i32, &str) {
+    let mut iterator = line.split(':');
+    let game_number = String::from(iterator.next().unwrap())
+        .split_off(5)
+        .parse::<i32>().unwrap();
+    let game_rounds = iterator.next().unwrap();
+    (game_number, game_rounds)
+}
+
+/*
+ * returns max count for each color (red, green, blue)
+ */
+fn get_max_numbers(game_rounds: &str) -> (i32, i32, i32) {
+    let mut blue_max = 0;
+    let mut red_max = 0;
+    let mut green_max = 0;
+
+    for round in game_rounds.split(';') {
+        for color in round.split(',') {
+            let mut iterator = color.split(' ');
+            iterator.next();
+            let color_count = String::from(iterator.next().unwrap()).parse::<i32>().unwrap();
+            let color_name = iterator.next().unwrap();
+            match color_name {
+                "blue" => if color_count > blue_max {
+                    blue_max = color_count;
+                },
+                "red" => if color_count > red_max {
+                    red_max = color_count;
+                },
+                "green" => if color_count > green_max {
+                    green_max = color_count;
+                },
+                _=> println!("What's happening...??!?!?"),
+            }
+        }
+    }
+    (red_max, green_max, blue_max)
+}
+
 fn part1(input: String) {
     println!("==================================================");
     println!("====== Part 1 ====================================");
@@ -24,33 +64,17 @@ fn part1(input: String) {
     let mut sum = 0;
 
     for line in input.lines() {
-        let mut iterator = line.split(':');
-        let game_number = String::from(iterator.next().unwrap()).split_off(5).parse::<i32>().unwrap();
-        let game_rounds = iterator.next().unwrap();
+        let game_number;
+        let game_rounds;
+        let red_max;
+        let blue_max;
+        let green_max;
+        (game_number, game_rounds) = get_number_and_rounds(line);
+        (red_max, green_max, blue_max) = get_max_numbers(game_rounds);
 
-        let mut valid = true;
-        for round in game_rounds.split(';') {
-            for color in round.split(',') {
-                let mut iterator = color.split(' ');
-                iterator.next();
-                let color_count = String::from(iterator.next().unwrap()).parse::<i32>().unwrap();
-                //let color_count = String::from(iterator.next().unwrap());
-                let color_name = iterator.next().unwrap();
-                match color_name {
-                    "blue" => if color_count > BLUE_MAX {
-                        valid = false;
-                    },
-                    "red" => if color_count > RED_MAX {
-                        valid = false;
-                    },
-                    "green" => if color_count > GREEN_MAX {
-                        valid = false;
-                    },
-                    _=> println!("What's happening...??!?!?"),
-                }
-            }
-        }
-        if valid {
+        if (red_max <= RED_MAX) &&
+            (green_max <= GREEN_MAX) &&
+            (blue_max <= BLUE_MAX) {
             println!("adding game number {}", game_number);
             sum += game_number;
         }
